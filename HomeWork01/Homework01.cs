a27e74e3a3de35117a32128a990a2cb83db0228c
DragonDJ <1812296895@qq.com>
2022-03-04 06:48:40 +0800

    first homework commit

---
+++ HomeWork01/Homework01.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace homework01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                char c = (char)Console.Read();
+
+                switch (c)
+                {
+                    case '+':
+                        Console.WriteLine(a + b);
+                        break;
+                    case '-':
+                        Console.WriteLine(a - b);
+                        break;
+                    case '/':
+                        Console.WriteLine(a / b);
+                        break;
+                    case '*':
+                        Console.WriteLine(a * b);
+                        break;
+                    default:
+                        Console.WriteLine("Input error!");
+                        break;
+                }
+            } catch (FormatException e) {
+                Console.WriteLine("Input exception!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
