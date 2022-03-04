c64d1dff166bd048de7d72e433391170cbe1d045
DragonDJ <1812296895@qq.com>
2022-03-05 07:25:56 +0800

    change the variable name, and add comment

--- HomeWork03/Program.cs
+++ HomeWork03/Program.cs
@@ -2,44 +2,46 @@
 
 namespace homework03
 {
-    // factory 
     class Program
     {
         static void Main(string[] args)
         {
-            Factory f = new Factory();
+            Factory factory = new Factory();
 
             Shape[] shapeList = new Shape[10];
             for (int i = 0; i < 10; i ++)
             {
                 Random rd = new Random();
-                int r = rd.Next(1, 5);
-                double r1 = Math.Abs(rd.NextDouble());
-                double r2 = Math.Abs(rd.NextDouble());
-                double r3 = Math.Abs(rd.NextDouble());
-                switch (r)
+	//创建一个随机整数来确定创建哪一个图形
+                int rm = rd.Next(1, 5);
+	//创建三个随机数来确定图形，为方便起见使随机数为正数
+                double rm1 = Math.Abs(rd.NextDouble());
+                double rm2 = Math.Abs(rd.NextDouble());
+                double rm3 = Math.Abs(rd.NextDouble());
+                switch (rm)
                 {
                     case 1:
-                        shapeList[i] = f.createShape("Circle", r1);
+                        shapeList[i] = factory.createShape("Circle", rm1);
                         break;
                     case 2:
-                        shapeList[i] = f.createShape("Rectangle", r1, r2);
+                        shapeList[i] = factory.createShape("Rectangle", rm1, rm2);
                         break;
                     case 3:
-                        shapeList[i] = f.createShape("Square", r1);
+                        shapeList[i] = factory.createShape("Square", rm1);
                         break;
                     case 4:
                         // 不能保证三角形合法
-                        shapeList[i] = f.createShape("Triangle", r1, r2, r3);
+                        shapeList[i] = factory.createShape("Triangle", rm1, rm2, rm3);
                         break;
                 }
             }
 
-            double sum = 0;
+            //累加图形的面积
+            double areaSum = 0;
             for (int i = 0; i < 10; i++)
-                sum = sum + f.getArea(shapeList[i]);
+                areaSum = areaSum + factory.getArea(shapeList[i]);
 
-            Console.WriteLine(sum);
+            Console.WriteLine(areaSum );
 
         }
     }
